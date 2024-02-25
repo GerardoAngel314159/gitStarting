3b079f68440a7ab013beb9df234f90ec1962a249
Gerardo Angel <angelmaldonadogerardo@gmail.com>
2024-02-25 17:54:54 -0600

    Version 2

---
+++ src/Empty.java
@@ -0,0 +1,10 @@
+class Empty{
+	public static void main (String [] args){
+		System.out.println("HelloWorld");
+        int i = 0;
+        while (i <= 2) {
+            i++;
+            System.out.println("Version" + i);	
+        }
+    }
+}
